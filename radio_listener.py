e73dfa50273e50e2a6994c73ca7b5df8729f32ff
Art Logos <artem1977gr@gmail.com>
2026-08-21 12:23:04 +0700

    Update radio_listener.py

--- radio_listener.py
+++ radio_listener.py
@@ -1,13 +1,16 @@
 import socket
-import ssl
+import ssl  # <-- Добавлен импорт для шифрования
 import time
 from urllib.parse import urlparse
 from multiprocessing import Process, current_process
 import random
 from datetime import datetime, timezone
-from collections import Counter
 
-# --- ВАШИ НАСТРОЙКИ ---
+
+# --- ВАШИ НАСТРОЙКИ (МЕНЯЙТЕ ЗДЕСЬ) ---
+
+# 1. Список станций. Дублируйте URL столько раз, сколько веса хотите дать станции.
+#    Порядок важен! Менеджер будет заполнять этот массив подряд.
 RADIOS = [
     *(['https://listen7.myradio24.com/sintezi'] * 31),
     *(['https://listen7.myradio24.com/nevermind'] * 30),
@@ -15,25 +18,21 @@ RADIOS = [
     *(['https://listen7.myradio24.com/iridium'] * 10),
     *(['https://listen7.myradio24.com/63908'] * 30)
 ]
-STATION_WEIGHTS = {
-    "https://listen7.myradio24.com/sintezi": 31,
-    "https://listen7.myradio24.com/nevermind": 30,
-    "https://listen7.myradio24.com/rockataka": 8,
-    "https://listen7.myradio24.com/iridium": 10,
-    "https://listen7.myradio24.com/63908": 30
-}
-TOTAL_BASE_WEIGHT = sum(STATION_WEIGHTS.values())
 REFERER_URL = "https://www.fmradiofree.com"
 SESSION_DURATION_MIN = 100   
 SESSION_DURATION_MAX = 1600  
 READ_TIMEOUT_SEC = 5        
 CHECK_INTERVAL_SEC = 300    
 GRACEFUL_STOP_DELAY = 120   
+
+# 2. Суточное расписание нагрузки (процент от длины массива RADIOS)
 TARGET_PERCENT_BY_HOUR = {
     0: 22, 1: 25, 2: 35, 3: 55, 4: 85, 5: 98, 6: 92, 7: 80,
     8: 75, 9: 78, 10: 76, 11: 74, 12: 77, 13: 82, 14: 90, 15: 100,
     16: 88, 17: 75, 18: 65, 19: 50, 20: 40, 21: 35, 22: 30, 23: 25
 }
+
+#### ⚡️ НАСТРОЙКИ РЕАЛИСТИЧНЫХ USER-AGENT'ОВ ###
 PLATFORM_WEIGHTS = [  
     {"os": "Windows", "version": "NT 10.0; Win64; x64", "weight": 0.1},  
     {"os": "Mac OS X", "version": "10_15_7", "weight": 0.05},
@@ -42,6 +41,7 @@ PLATFORM_WEIGHTS = [
     {"os": "Linux", "version": "x86_64", "weight": 0.05},
     {"os": "X11", "version": "Ubuntu; Linux x86_64", "weight": 0.05}
 ]
+
 BROWSER_WEIGHTS = [  
     {"name": "Chrome", "version": "129.0.0.0", "weight": 0.6},
     {"name": "Firefox", "version": "121.0", "weight": 0.2},
@@ -50,13 +50,16 @@ BROWSER_WEIGHTS = [
     {"name": "Opera", "version": "98.0.4825.16", "weight": 0.05}
 ]
 
+
 def generate_user_agent():
     total_weight_platforms = sum(item["weight"] for item in PLATFORM_WEIGHTS)
     choice = random.uniform(0, total_weight_platforms)
     platform_data = next(plat for plat in PLATFORM_WEIGHTS if (choice := choice - plat["weight"]) < 0)
+
     total_weight_browsers = sum(item["weight"] for item in BROWSER_WEIGHTS)
     choice = random.uniform(0, total_weight_browsers)
     browser_data = next(brw for brw in BROWSER_WEIGHTS if (choice := choice - brw["weight"]) < 0)
+
     ua_template = (
         f"Mozilla/5.0 ({platform_data['os']} {platform_data.get('version', '')}; "
         f"{platform_data.get('arch', '')} {platform_data.get('model', '')}) "
@@ -66,32 +69,38 @@ def generate_user_agent():
     )
     return ua_template.strip()
 
+
 def keep_radio_alive(url):
     parsed_url = urlparse(url)
     host = parsed_url.netloc.split(':')[0] 
     path = parsed_url.path or '/'
+
     headers = (
         f"GET {path} HTTP/1.1\r\n"
-        f"Host: {host}:443\r\n"
+        f"Host: {host}:443\r\n"  # Ключевое исправление: порт 443 в Host
         f"Icy-MetaData: 1\r\n"
         f"User-Agent: {generate_user_agent()}\r\n"
         f"Referer: {REFERER_URL}\r\n"
         f"Connection: Keep-Alive\r\n"
         "\r\n"
     )
+
     while True:  
         session_duration = random.randint(SESSION_DURATION_MIN, SESSION_DURATION_MAX)
         try:
             raw_sock = socket.create_connection((host, 443))
             sock = ssl.wrap_socket(raw_sock)
+            
             sock.settimeout(READ_TIMEOUT_SEC)
             sock.sendall(headers.encode())
+            
             response_headers = b""
             while True:
                 chunk = sock.recv(4096)
                 if not chunk or b"\r\n\r\n" in response_headers:
                     break
                 response_headers += chunk
+
             start_time = time.time()
             proc_name = current_process().name
             while int(time.time() - start_time) < session_duration:
@@ -110,72 +119,45 @@ def keep_radio_alive(url):
             except Exception:
                 pass
 
+
 def get_target_listeners_for_now():
     utc_hour = datetime.now(timezone.utc).hour
     percent = TARGET_PERCENT_BY_HOUR[utc_hour]
-    target_count = int(TOTAL_BASE_WEIGHT * (percent / 100))
-    return target_count, STATION_WEIGHTS
+    
+    # База берется из физического количества элементов в RADIOS (сумма весов)
+    base_listeners = len(RADIOS)
+    target_count = int(base_listeners * (percent / 100))
+    return target_count
+
 
 def scheduler_manager(active_processes):
-    # Фильтруем ТОЛЬКО живые процессы от тех, что уже умерли сами собой
     alive_processes = [p for p in active_processes if p.is_alive()]
     
-    new_target, station_weights = get_target_listeners_for_now()
-    max_possible_listeners = TOTAL_BASE_WEIGHT
-    target_count = min(new_target, max_possible_listeners)
+    new_target = get_target_listeners_for_now()
     current_live = len(alive_processes)
 
-    live_urls = []
-    for p in alive_processes:
-        try:
-            live_urls.append(p._args[0])
-        except (AttributeError, IndexError):
-            continue
-            
-    current_counts = Counter(live_urls)
-
-    targets_per_station = {}
-    for station_url, weight in station_weights.items():
-        ideal_count = int(target_count * (weight / TOTAL_BASE_WEIGHT))
-        targets_per_station[station_url] = ideal_count
-
     #### ДОБОР ПРОЦЕССОВ ####
-    if to_spawn := target_count - current_live > 0:
-        spawn_queue = []
-        for station_url, ideal_count in targets_per_station.items():
-            have_now = current_counts.get(station_url, 0)
-            need_for_this_station = ideal_count - have_now
-            if need_for_this_station > 0:
-                spawn_queue.extend([station_url] * need_for_this_station)
-                if len(spawn_queue) >= to_spawn:
-                    break
+    if to_spawn := new_target - current_live > 0:
+        # Находим свободные ИНДЕКСЫ в глобальном массиве RADIOS
+        free_slots = set(range(len(RADIOS))) - {active_processes.index(p) for p in alive_processes if p in active_processes}
         
-        for radio_url in spawn_queue[:to_spawn]:
+        slots_to_fill = random.sample(list(free_slots), min(to_spawn, len(free_slots)))
+        for slot_index in slots_to_fill:
+            radio_url = RADIOS[slot_index]
             p = Process(target=keep_radio_alive, args=(radio_url,))
             p.start()
-            # Добавляем НАПРЯМУЮ в живой список
-            alive_processes.append(p)
-            print(f"[MANAGER] Spawned listener -> {radio_url}")
+            # Вставляем новый процесс СТРОГО в его слот внутри общего списка
+            active_processes.insert(slot_index, p)
+            print(f"[MANAGER] Spawned listener #{slot_index} -> {radio_url}")
 
     #### УДАЛЕНИЕ ЛИШНИХ ПРОЦЕССОВ ####
-    elif current_live > target_count:
+    elif current_live > new_target:
         processes_to_kill = []
+        # Идем с конца списка, чтобы не сбивать индексы при удалении
         for i in range(len(alive_processes) - 1, -1, -1):
-            if len(processes_to_kill) >= current_live - target_count:
+            if len(processes_to_kill) >= current_live - new_target:
                 break
-            
-            p = alive_processes[i]
-            try:
-                proc_url = p._args[0]
-            except (AttributeError, IndexError):
-                continue
-
-            have_now = current_counts.get(proc_url, 0)
-            ideal_count = targets_per_station.get(proc_url, 0)
-            
-            if have_now > ideal_count:
-                processes_to_kill.append(p)
-                current_counts[proc_url] -= 1
+            processes_to_kill.append(alive_processes[i])
 
         for p in processes_to_kill:
             if p.is_alive():
@@ -184,30 +166,36 @@ def scheduler_manager(active_processes):
                 if p.is_alive():
                     p.kill()
         
-        # Удаляем убитые процессы ИЗ живого списка
-        alive_processes = [p for p in alive_processes if p not in processes_to_kill]
+        # Удаляем убитые процессы из основного списка
+        active_processes = [p for p in active_processes if p not in processes_to_kill]
 
-    return alive_processes
+    return active_processes
 
 
 if __name__ == "__main__":
     manager_active = []
     
-    initial_target, _ = get_target_listeners_for_now()
+    initial_target = get_target_listeners_for_now()
     print(f"[INIT] Starting at {initial_target} listeners based on current UTC hour.")
     
-    # ПЕРВОНАЧАЛЬНЫЙ ЗАПУСК
-    # Присваиваем результат менеджеру!
+    # Первоначальный запуск
     manager_active = scheduler_manager(manager_active)
 
     try:
         while True:
             time.sleep(CHECK_INTERVAL_SEC)
-            new_target, _ = get_target_listeners_for_now()
             
-            # ГЛАВНАЯ СТРОЧКА: мы берем возвращенный СПИСОК и полностью заменяем им старый
-            manager_active = scheduler_manager(manager_active)
+            new_target = get_target_listeners_for_now()
+            alive_processes = [p for p in manager_active if p.is_alive()]
             
+            # Обновляем главный список актуальными живыми процессами
+            manager_active = alive_processes
+            
+            if len(alive_processes) != new_target:
+                timestamp = time.strftime('%H:%M:%S')
+                print(f"[{timestamp}] Schedule change detected. Target: {new_target}, Live: {len(alive_processes)}. Adjusting...")
+                manager_active = scheduler_manager(alive_processes)
+
     except KeyboardInterrupt:
         print("\n[MAIN] Shutdown signal received. Terminating all processes...")
         for p in manager_active:
